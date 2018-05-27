d467a26003f9916f07b051f53df5bc5e447030dd
simonoppowa <simon.oppowa@st.oth-regensburg.de>
2018-05-27 11:50:19 +0200

    added Ingredients to RecipeActivity

---
+++ app/src/main/java/com/github/simonoppowa/bakingapp/IngredientAdapter.java
@@ -0,0 +1,67 @@
+package com.github.simonoppowa.bakingapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.simonoppowa.bakingapp.model.Ingredient;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
+
+    private final Context context;
+
+    private List<Ingredient> mIngredientList;
+
+    public IngredientAdapter(Context context, List<Ingredient> ingredientList) {
+        this.context = context;
+        this.mIngredientList = ingredientList;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingredient_card_item, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.ingredientNameTextView.setText(mIngredientList.get(position).getIngredientName());
+        holder.ingredientQuantity.setText(mIngredientList.get(position).getQuantity());
+        holder.ingredientMeasureTextView.setText(mIngredientList.get(position).getMeasure());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mIngredientList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.ingredient_name_TextView)
+        TextView ingredientNameTextView;
+
+        @BindView(R.id.ingredient_quantity_TextView)
+        TextView ingredientQuantity;
+
+        @BindView(R.id.ingredient_measure_TextView)
+        TextView ingredientMeasureTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/github/simonoppowa/bakingapp/RecipeActivity.java
+++ app/src/main/java/com/github/simonoppowa/bakingapp/RecipeActivity.java
@@ -3,15 +3,27 @@ package com.github.simonoppowa.bakingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 import com.github.simonoppowa.bakingapp.model.Recipe;
 
+import java.util.Arrays;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
 import static com.github.simonoppowa.bakingapp.MainActivity.*;
 
 public class RecipeActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.ingredient_card_recyclerView)
+    RecyclerView mIngredientsRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private IngredientAdapter mIngredientAdapter;
 
     private Recipe mRecipe;
 
@@ -29,11 +41,20 @@ public class RecipeActivity extends AppCompatActivity {
 
         mRecipe = recipeIntent.getParcelableExtra(RECIPE_KEY);
 
-        if(mRecipe == null) {
+        if (mRecipe == null) {
             throw new NullPointerException("No Recipe was passed to RecipeActivity");
         }
 
         //set Title
         setTitle(mRecipe.getName());
+
+        //creating RecyclerView
+        mLinearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mIngredientAdapter = new IngredientAdapter(this, Arrays.asList(mRecipe.getIngredients()));
+
+        mIngredientsRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mIngredientsRecyclerView.setAdapter(mIngredientAdapter);
+
+
     }
 }
