d55b6fda2c8a03654ae459e59e370dc2531efdae
simonoppowa <simon.oppowa@st.oth-regensburg.de>
2018-05-29 17:27:04 +0200

    added ExoPlayer RecipeStepActivity

--- app/src/main/java/com/github/simonoppowa/bakingapp/RecipeStepActivity.java
+++ app/src/main/java/com/github/simonoppowa/bakingapp/RecipeStepActivity.java
@@ -1,17 +1,47 @@
 package com.github.simonoppowa.bakingapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.simonoppowa.bakingapp.model.RecipeStep;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
 import static com.github.simonoppowa.bakingapp.RecipeActivity.RECIPE_STEP_KEY;
 
 public class RecipeStepActivity extends AppCompatActivity {
+
+    private SimpleExoPlayer mSimpleExoPlayer;
+
+    @BindView(R.id.recipe_step_exo_player)
+    SimpleExoPlayerView mSimpleExoPlayerView;
+
+    private Timeline.Window mWindow;
+    private DataSource.Factory mMediaDataSourceFactor;
+    private DefaultTrackSelector mTrackSelector;
+    private boolean shouldAutoPlay;
+    private BandwidthMeter mBandwidthMeter;
 
     private RecipeStep mRecipeStep;
 
@@ -36,5 +66,42 @@ public class RecipeStepActivity extends AppCompatActivity {
         //setting title
         String title = (mRecipeStep.getId() + 1) + ". " + mRecipeStep.getShortDescription();
         setTitle(title);
+
+        //setting up Player
+        shouldAutoPlay = true;
+        mBandwidthMeter = new DefaultBandwidthMeter();
+        mMediaDataSourceFactor = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this,
+                        "BakingApp"),
+                (TransferListener<? super DataSource>) mBandwidthMeter);
+        mWindow = new Timeline.Window();
+
+        if(mRecipeStep.getVideoURL() != null && !mRecipeStep.getVideoURL().isEmpty()) {
+            setUpExoPlayer();
+        }
+
+    }
+
+    private void setUpExoPlayer() {
+
+        mSimpleExoPlayerView.requestFocus();
+
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(mBandwidthMeter);
+
+        mTrackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        mSimpleExoPlayer = ExoPlayerFactory.newSimpleInstance(this, mTrackSelector, new DefaultLoadControl());
+
+        mSimpleExoPlayerView.setPlayer(mSimpleExoPlayer);
+
+        mSimpleExoPlayer.setPlayWhenReady(shouldAutoPlay);
+
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(mRecipeStep.getVideoURL()),
+                mMediaDataSourceFactor, extractorsFactory, null, null);
+
+        mSimpleExoPlayer.prepare(mediaSource);
     }
 }
